56bc0ed7b660b91ca627e3d4a74940956ae1fe32
ajaypundir04 <ajaysinghpundir70@gmail.com>
2023-10-19 19:30:38 +0200

    jimp

--- __test__/compare-image-test.js
+++ __test__/compare-image-test.js
@@ -3,13 +3,19 @@ const comparePixel = require('../main');
 
 
 describe('Image Comparison', () => {
-  it('should return true for equal images', async () => {
+  test('should return true for equal images for jpg', async () => {
+    const url1 = 'https://picsum.photos/200/300.jpg';
+    const url2 = 'https://picsum.photos/200/300.jpg';
+    const result = await comparePixel.downloadAndCompareImages(url1, url2);
+    expect(result).toBe(true)
+  });
+
+  test('should return true for equal images for png', async () => {
     const url1 = 'https://www.gstatic.com/webp/gallery3/1.sm.png';
     const url2 = 'https://www.gstatic.com/webp/gallery3/1.sm.png';
     const result = await comparePixel.downloadAndCompareImages(url1, url2);
     expect(result).toBe(true)
   });
-
 
 
  
